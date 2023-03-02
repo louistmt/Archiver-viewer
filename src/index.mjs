6b97674365e27816bfbd078dcd254b0b312da2e5
louistmt <apenaspara1998@gmail.com>
2023-03-02 13:06:26 +0000

    Making the app actually run

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,10 @@
 import express from "express";
 
+const PORT = 9090
 const app = express();
 
 app.use("/static", express.static("static"))
+
+app.listen(PORT, () => {
+    console.log(`App is up at port ${PORT}`);
+})
