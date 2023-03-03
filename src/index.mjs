d18bc20c808fec3dee2f841615e1e8ed692a1a48
louistmt <apenaspara1998@gmail.com>
2023-03-03 00:03:36 +0000

    Server functionality
    Essentially three routes. One for the viewer, anoter for the api and another for the static files

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,27 @@
-import express from "express";
+import { request } from "https"
+import express from "express"
 
 const PORT = 9090
-const app = express();
+const app = express()
+const staticFilesPath = import.meta.url.replace("file:///", "").replace("src/index.mjs", "static/")
 
-app.use("/static", express.static("static"))
+app.set("title", "Archive Viewer")
+app.use("/static", express.static(staticFilesPath))
+
+app.get("/viewer/:channelId/:fileId/:fileName", (req, res) => {
+    res.sendFile("viewer.html", {root: staticFilesPath})
+})
+
+app.get("/api/:channelId/:fileId/:fileName", (req, res) => {
+    const {channelId, fileId, fileName} = req.params;
+    const cdnReq = request(`https://cdn.discordapp.com/attachments/${channelId}/${fileId}/${fileName}`);
+    cdnReq.on("response", (cdnRes) => {
+        res.type("application/json")
+        cdnRes.pipe(res)
+    });
+    cdnReq.end()
+})
 
 app.listen(PORT, () => {
-    console.log(`App is up at port ${PORT}`);
+    console.log(`App is up at port ${PORT}`)
 })
